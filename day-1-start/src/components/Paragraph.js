d24321558a251d7ceea09048f6149d2dc0782b2a
Hesham <heshamelmasry1992@gmail.com>
2023-02-21 14:38:07 +0100

    day 1 start

---
+++ day-1-start/src/components/Paragraph.js
@@ -0,0 +1,14 @@
+function Paragraph() {
+  return (
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis tincidunt est, id rhoncus mi
+        pulvinar et. Ut efficitur lacus eget lorem pellentesque rutrum. Etiam id tincidunt sem, sit amet facilisis nisi.
+        Vestibulum aliquet erat sit amet cursus pellentesque. Integer rhoncus, nunc ut bibendum dignissim, ante nulla
+        rhoncus odio, ut tempus felis purus suscipit ligula. Pellentesque vitae pellentesque ante. Phasellus augue
+        turpis, molestie non maximus quis, pellentesque ut lacus. Phasellus fermentum arcu venenatis sapien fermentum,
+        sit amet mollis ante aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+        curae; Ut dignissim lacus vel est tempor placerat. Pellentesque faucibus et urna a pharetra.
+      </p>
+  )
+}
+
+export default Paragraph;
